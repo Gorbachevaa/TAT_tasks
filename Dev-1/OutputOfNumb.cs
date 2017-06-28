56a0b4e5838a2d78b96b33a4b5a293eb3eb38c94
Gorbachevaa <anya_gorbacheva96@mail.ru>
2017-06-28 14:50:41 +0300

    Modified task DEV-1. 'Tutti-frutti'.

--- Dev-1/OutputOfNumb.cs
+++ Dev-1/OutputOfNumb.cs
@@ -5,19 +5,29 @@ namespace Dev_1
     // Class that realize output of all numbers by the task.
     class OutputOfNumb
     {
-        // Method that outputs the requried sequence.
-        public void Output(int n, int mult)
+        // Method that outputs the required sequence.
+        public void Output(int n, int mult1, int mult2)
         {
             MultipleNumb multipleNumb = new MultipleNumb();
-            
+
             for (var i = 0; i < n + 1; i++)
             {
-                if (!multipleNumb.isMultiple(i,mult))
+                if (multipleNumb.isMultiple(i, mult1) && multipleNumb.isMultiple(i, mult2))
                 {
-                    Console.WriteLine(i);
+                    Console.WriteLine("Tutti-Frutti");
                 }
                 else
-                    Console.WriteLine(mult + "*" + i / mult);
+                    if (multipleNumb.isMultiple(i, mult1))
+                    {
+                        Console.WriteLine("Tutti");
+                    }
+                    else
+                        if (multipleNumb.isMultiple(i, mult2))
+                        {
+                            Console.WriteLine("Frutti");
+                        }
+                        else
+                            Console.WriteLine(i);
             }
         }
     }
