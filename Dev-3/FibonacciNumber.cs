b52e8cab655e68e622d0fffd29877455c4ea0d9c
Gorbachevaa <anya_gorbacheva96@mail.ru>
2017-06-28 15:00:50 +0300

    Check fibonacci number.

---
+++ Dev-3/FibonacciNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Dev_3
+{
+    // Class that realizes Fibonacci number.
+    class FibonacciNumber
+    {
+        // Method that checks whether the number is a Fibonacci.
+        public bool IsFibbonacci(uint number)
+        {
+            const int n = 100;
+            uint[] F = new uint[n];
+            F[0] = 0;
+            F[1] = 1;
+            int i = 2;
+            while (number > F[i-1])
+            {
+                F[i] = F[i - 1] + F[i - 2];
+                i++;
+            }
+            if (number == F[0])
+                return true;
+            else
+                return number == F[i - 1];
+        }
+
+    }
+}
---
+++ Dev-3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Dev_3
+{
+    class Program
+    {
+        // Program that checks whether number is a Fibonacci number.
+        static void Main(string[] args)
+        {
+            try
+            {
+                uint number;
+                FibonacciNumber fibNumb = new FibonacciNumber();
+                Console.WriteLine(" Enter the number: ");
+                // Checks the correctness of the number
+                number = Convert.ToUInt32(Console.ReadLine(), 10);
+                Console.WriteLine(" The number " + number + " is a Fibbonacci number? ");
+                Console.WriteLine(fibNumb.IsFibbonacci(number));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid format of input number. ");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine(" The number is too large or negative. ");
+            }
+            finally
+            {
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
