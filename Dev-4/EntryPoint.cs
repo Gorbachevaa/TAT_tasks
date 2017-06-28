b2ddb148a511903cb4ead64365508914e491e8fb
Gorbachevaa <anya_gorbacheva96@mail.ru>
2017-06-28 15:06:48 +0300

    Check nondecreasing sequence.

---
+++ Dev-4/EntryPoint.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DEV_4
+{
+    // A program that determines that the user-entered sequence of integers is nondecreasing. 
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                try
+                {
+                    Sequence nondecreaseSeq = new Sequence();
+                    int[] seq;
+                    seq = nondecreaseSeq.numbTranslater();
+                    Console.WriteLine("The entered sequence is a nondecreasing sequence? ");
+                    Console.WriteLine(nondecreaseSeq.isNondecreaseSequence(seq));
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Error! You entered invalid format of number or unnessesary space.");
+                }
+                finally
+                {
+                    Console.WriteLine("Press 'esc' to exit. Otherwise press any key to continue.");
+                }
+            }
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ Dev-4/Sequence.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DEV_4
+{
+    class Sequence
+    {
+        // Method that translaste entered numbers into an array of integer values.
+        public int[] numbTranslater()
+        {
+            Console.Write("Enter a sequence of number with a space.");
+            Console.WriteLine("Enter one space between numbers. ");
+            Console.WriteLine("To finish input of number press Enter: ");
+            string[] tokens = Console.ReadLine().Split();
+            int[] number = new int[tokens.Length];
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                number[i] = Convert.ToInt32(tokens[i], 10);
+            }
+            return number;
+        }
+
+        // Method that checks if sequence is nondecreasing.
+        public bool isNondecreaseSequence(int[] number)
+        {
+            bool isNondecrease = true;
+            int i = 1;
+            int n = number.Length;
+            while (i < n)
+            {
+                if (number[i] < number[i - 1])
+                {
+                    isNondecrease = false;
+                    break;
+                }
+                i++;
+            } 
+            return isNondecrease;
+        }
+    }
+}
