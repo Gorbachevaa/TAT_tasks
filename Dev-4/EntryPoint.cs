f0d2f65b9d84e5f2fad210f566d308d5869bfbee
Gorbachevaa <anya_gorbacheva96@mail.ru>
2017-07-10 18:26:31 +0300

    Renamed method and object of class.

--- Dev-4/EntryPoint.cs
+++ Dev-4/EntryPoint.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace DEV_4
+namespace DEV_5
 {
     // A program that determines that the user-entered sequence of integers is nondecreasing. 
     // User could pass sequense as command-line argument. 
@@ -9,45 +9,24 @@ namespace DEV_4
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             const string QUESTION = "The entered sequence is a nondecreasing sequence? ";
             const string EXITPROPMT = "Press 'esc' to exit. Otherwise press any key to continue.";
             const string ERRMESSAGE = "Error! You entered invalid format of number or unnessesary space.";
             const string SOLUTIONFORERROR = "Please, try again.";
-=======
-            const string TASKPROMPT = "The entered sequence is a nondecreasing sequence?";
-            const string ERRORMSG = "Error! You entered invalid format of number or unnessesary space.";
-            const string EXITPROMPT = "Press 'esc' to exit. Or try again( Press any symbol.).";
->>>>>>> 27da1d6aa8efd70b1e120e3e016993135d11e296
             do
             {
                 try
                 {
-<<<<<<< HEAD
-                    Sequence seq = new Sequence();
-                    seq.EnterViaCmdOrConsole(args);
-                    Console.WriteLine(QUESTION);
-                    Console.WriteLine(seq.IsNondecrease());
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine(ERRMESSAGE);
-                    Console.WriteLine(SOLUTIONFORERROR);
-                    Console.ReadKey();
-=======
                     Sequence sequence = new Sequence();
-                    sequence.TranslateEnteredNumbers();
-                    Console.WriteLine(TASKPROMPT);
+                    sequence.EnterViaCmdOrConsole(args);
+                    Console.WriteLine(QUESTION);
                     Console.WriteLine(sequence.IsNonDecrease());
                 }
                 catch (Exception)
                 {
-                    Console.WriteLine(ERRORMSG);
-                }
-                finally
-                {
-                    Console.WriteLine(EXITPROMPT);
->>>>>>> 27da1d6aa8efd70b1e120e3e016993135d11e296
+                     Console.WriteLine(ERRMESSAGE);
+                    Console.WriteLine(SOLUTIONFORERROR);
+                    Console.ReadKey();
                 }
                 Console.WriteLine(EXITPROPMT);                    
             }
--- Dev-4/Sequence.cs
+++ Dev-4/Sequence.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 
-namespace DEV_4
+namespace DEV_5
 {
     class Sequence
     {
         const string INPUTPROMPT = "Enter a sequence of number with a one space. ";
         const string CONDITIONTOFINISH = "To finish input of number press Enter: ";
-<<<<<<< HEAD
         const string CMDPROMPT = "String from command line: ";
         public int[] Number { get; set; }
         // Method that determines how sequence is entered and returns integer value of string.
@@ -17,8 +16,7 @@ namespace DEV_4
                 Console.Write(INPUTPROMPT);
                 Console.WriteLine(CONDITIONTOFINISH);
                 string[] tokens = Console.ReadLine().Split();
-                int[] numbSequence = StrTranslater(tokens);
-                Number = numbSequence;
+                Number = StrTranslater(tokens);
             }
             else
             {
@@ -28,49 +26,25 @@ namespace DEV_4
                     Console.Write(args[i] + " ");
                 }
                 Console.WriteLine();
-                int[] numbSequence = StrTranslater(args);
-                Number = numbSequence;
+                Number = StrTranslater(args);
             }
         }
-
-        // Method that translastes entered numbers into an array of integer values.
+        // Method that translaste entered numbers into an array of integer values.
         public int[] StrTranslater(string[] tokens)
         {
             int[] number = new int[tokens.Length];
-=======
-        public int[] Number { get; set; }
-        
-        // Method that translaste entered numbers into an array of integer values.
-        public void TranslateEnteredNumbers()
-        {
-            Console.Write(INPUTPROMPT);
-            Console.WriteLine(CONDITIONTOFINISH);
-            string[] tokens = Console.ReadLine().Split();
-            Number = new int[tokens.Length];
->>>>>>> 27da1d6aa8efd70b1e120e3e016993135d11e296
             for (int i = 0; i < tokens.Length; i++)
             {
-                Number[i] = Convert.ToInt32(tokens[i], 10);
+                number[i] = Convert.ToInt32(tokens[i], 10);
             }
+            return number;
         }
 
         // Method that checks if sequence is nondecreasing.
-<<<<<<< HEAD
-        public bool IsNondecrease()
-        {
-            int[] number = Number;
-            bool isNondecrease = true;
-            if (number.Length == 1)
-            {
-                isNondecrease = false;
-            }
-            for (int i = 1; i < number.Length; i++ )
-=======
         public bool IsNonDecrease()
         {
             bool isNonDecrease = true;
             if (Number.Length == 1)
->>>>>>> 27da1d6aa8efd70b1e120e3e016993135d11e296
             {
                 isNonDecrease = false;
             }
@@ -81,13 +55,8 @@ namespace DEV_4
                     isNonDecrease = false;
                     break;
                 }
-<<<<<<< HEAD
             }
-            return isNondecrease;
-=======
-            } 
             return isNonDecrease;
->>>>>>> 27da1d6aa8efd70b1e120e3e016993135d11e296
         }
     }
 }
