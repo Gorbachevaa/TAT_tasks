2f4d54f90fcb573b71e45424e236e14e1c296c49
Gorbachevaa <anya_gorbacheva96@mail.ru>
2017-09-29 11:42:29 +0300

    Task that replaces subsequences.

---
+++ Dev-9/EntryPoint.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Dev_9
+{
+    // A programm that replace random subsequence in one line
+    // by random subsequence from the other.
+    // The console displays original strings and the modified. 
+    class EntryPoint
+    {
+        const string TEXTFORFIRSTSTRING = "First original string: ";
+        const string TEXTFORCHANGINGSTRING = "Second original string: ";
+        const string TEXTFORCHANGEDSTRING = "Changed string: ";
+        static void Main(string[] args)
+        {
+            SymbolsReplacement symbolReplacement = new SymbolsReplacement();
+            symbolReplacement.Replacer();
+            Console.Write(TEXTFORFIRSTSTRING);
+            symbolReplacement.Outputer(symbolReplacement.NotChangingSequence);
+            Console.Write(TEXTFORCHANGINGSTRING);
+            symbolReplacement.Outputer(symbolReplacement.ChangingSequence);
+            Console.Write(TEXTFORCHANGEDSTRING);
+            symbolReplacement.Outputer(symbolReplacement.ChangedSequence);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Dev-9/FileChecker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace Dev_9
+{
+    // Class checks file and defines sequences.
+    class FileChecker
+    {
+        public string[] SequencesFromFile { get; set; }
+        // The amount of sequences in file.
+        private int amount;
+        public int Amount
+        {
+            get
+            {
+                return amount;
+            }
+            set
+            {
+                amount = value;
+            }
+        }
+        // Method reads data from file, returns an array of original sequences.
+        public string[] Read(string filename)
+        {
+            StreamReader fs = new StreamReader(filename);
+            Amount = 2;
+            SequencesFromFile = new string[Amount];
+            int i = 0;
+            while (true)
+            {
+                string temp = fs.ReadLine();
+                if (temp == null)
+                {
+                    break;
+                }
+                SequencesFromFile[i] = temp;
+                i++;
+            }
+            return SequencesFromFile;
+        }
+    }
+}
+
