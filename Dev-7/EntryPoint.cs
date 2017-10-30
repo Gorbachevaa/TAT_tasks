40b56dd4c0c2362be9b175788040cf24d4ea95f2
Gorbachevaa <anya_gorbacheva96@mail.ru>
2017-10-30 09:40:04 +0300

    Update class EntryPoint.

--- Dev-7/EntryPoint.cs
+++ Dev-7/EntryPoint.cs
@@ -1,12 +1,61 @@
 ﻿using System;
 
-
 namespace Dev_7
 {
+    /// <summary>
+    /// Class contains the entry point of rhe programm.
+    /// </summary>
     class EntryPoint
     {
+        const string INVALID_SIDES = "Triangle's sides should be positive.Try enter valid numbers.";
+        const string TRIANGLE_EXISTANCE = "Triangle doesn't exist.Try enter right numbers.";
+        const string FORMAT_ESCEPTION = "Only numbers are needed. Please enter them.";
+        const string EXIT_OR_CONTINUE = "Press 'esc' to exit. Otherwise press any key to continue.";
+        /// <summary>
+        /// First sides of a triangle is inputed.
+        /// Then Checker method is called. It checks if sides is valid(> 0) and if sides form the triangle.
+        /// Then class Builder is appeared. It determines the type of the triangle and return the type.
+        /// And the end of the program the type is outputed to the console.
+        /// If the sides is invalid, sides don't form the triangle or the user wants to check another
+        /// triangle, then the program is continued.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            bool continueProgram = true;
+            while (continueProgram)
+            {
+                try
+                {
+                    double[] sides = new SidesInputer().SidesInput();
+                    Checker checker = new Checker();
+                    if (!checker.IsValidSides(sides))
+                    {
+                        Console.WriteLine(INVALID_SIDES);
+                        continue;
+                    }
+                    if (!checker.IsTriangle(sides))
+                    {
+                        Console.WriteLine(TRIANGLE_EXISTANCE);
+                        continue;
+                    }
+                    Builder builder = new Builder(sides);
+                    Triangle triangle = builder.Build(sides);
+                    Console.WriteLine(triangle.GetTrType());
+                    Console.WriteLine(EXIT_OR_CONTINUE);
+                    if(Console.ReadKey(true).Key != ConsoleKey.Escape)
+                    { 
+                        continue;
+                    }
+                    Console.ReadKey();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine(FORMAT_ESCEPTION);
+                    continue;
+                }
+                continueProgram = false;
+            }
         }
     }
 }
