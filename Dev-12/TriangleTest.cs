433c5cabbe3d9d2f9e6ed5e7c6f8faa925150630
Gorbachevaa <anya_gorbacheva96@mail.ru>
2017-10-31 03:01:00 +0300

    TriangleTest was added. Class with unit-tests.

---
+++ Dev-12/TriangleTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dev_12
+{
+    [TestClass]
+    public class TriangleTest
+    {
+        [TestMethod]
+        public void SidesInput_1_and_2_and_3_123returned()
+        {
+        }
+    }
+}
