5e64b3894893557b72a1996e0213b95dc3774e54
Gorbachevaa <anya_gorbacheva96@mail.ru>
2017-11-08 17:09:29 +0300

    Update Employee class and its child classes.

---
+++ Dev-13/Employees/Lead.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Dev_13
+{
+    class Lead : Employee
+    {
+        public const string LEAD = "Lead";
+        public Lead(int i) : base() 
+        {
+            Salary = 300;
+            Productivity = 660;
+            Priority = 3;
+            if (i != 3)
+            {
+                throw new EmployeeTypeException();
+            }
+        }
+        public override string GetEmployeeType()
+        {
+            return LEAD;
+        }
+        public override int[] GetEmployeeInfo()
+        {
+            return new int[] { Salary, Productivity };
+        }
+    }
+}
