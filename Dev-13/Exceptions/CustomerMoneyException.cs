4588972fc26b08da54144e955f40e4df4ae6047c
Gorbachevaa <anya_gorbacheva96@mail.ru>
2017-11-08 17:11:01 +0300

    Add exception classes.

---
+++ Dev-13/Exceptions/CustomerMoneyException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Dev_13
+{
+    /// <summary>
+    /// Class inherited by Exception.  Catch exception when there are not enough customer's money.
+    /// </summary>
+    class CustomerMoneyException : Exception
+    {
+        public CustomerMoneyException() : 
+            base("You have not enough money. We can't help you.") { }
+
+    }
+}
---
+++ Dev-13/Exceptions/DeveloperBrainException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Dev_13
+{
+    class DeveloperBrainException : Exception
+    {
+        public DeveloperBrainException() :
+            base("Here should be some variant, but a programmer didn't right it.") { }
+    }
+}
