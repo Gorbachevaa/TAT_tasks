d8db7d9df4cdba6bb2634e041d7c26c6c4e1e1fa
Gorbachevaa <anya_gorbacheva96@mail.ru>
2017-11-08 17:10:25 +0300

    Update handlers and add CriterionHandler.

---
+++ Dev-13/Handlers/JuniorHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Dev_13
+{
+    /// <summary>
+    /// Class handles Junior's employee.
+    /// </summary>
+    class JuniorHandler : EmployeeHandler
+    {
+        public JuniorHandler(EmployeeHandler successor) 
+        {
+            this.successor = successor;
+        }
+        /// <summary>
+        /// Creates new Employee called Junior.
+        /// </summary>
+        /// <param name="employeeInfo"></param>
+        /// <returns> New Junior's object or Exception.</returns>
+        public override Employee Handle(int priority)
+        {
+            try
+            {
+                return new Junior(priority);
+            }
+            catch (EmployeeTypeException)
+            {
+                if (successor != null)
+                {
+                    return successor.Handle(priority);
+                }
+                throw new EmployeeHandleException();
+            }
+        }
+    }
+}
