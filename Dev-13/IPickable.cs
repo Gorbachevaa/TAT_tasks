0480916bab5c9e221e8ca7b5b739a3e1e0e2036d
Gorbachevaa <anya_gorbacheva96@mail.ru>
2017-11-08 17:13:43 +0300

    Add Strategy Pattern.

---
+++ Dev-13/IPickable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dev_13
+{
+    /// <summary>
+    /// Interface for picking a employee's team.
+    /// </summary>
+    interface IPickable
+    {
+        int[] PickATeam(int[] customerInfo);
+    }
+}
---
+++ Dev-13/MaxProductivityPick.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Dev_13
+{
+    /// <summary>
+    /// Picking a team with the first selection criterion.
+    /// (Maximum productivity at a fixed price)
+    /// </summary>
+    public class MaxProductivityPick : IPickable
+    {
+        public const int EMPLOYEES = 4;
+
+        public int[] EmployeesAmount { get; set; }
+        public int[] PickATeam(int[] customerInfo)
+        {
+            Manager manager = new Manager();
+            int[] employeeSalary = manager.GetEmployeesSalary();
+            int customerSumma = customerInfo[0];
+            EmployeesAmount = new int[] { 0, 0, 0, 0 };
+            while (customerSumma > 0)
+            {
+                if (customerSumma >= employeeSalary[3])
+                {
+                    customerSumma -= employeeSalary[3];
+                    EmployeesAmount[3] += 1;
+                }
+                else if ((customerSumma <= employeeSalary[3]) && (customerSumma >= employeeSalary[2]))
+                {
+                    customerSumma -= employeeSalary[2];
+                    EmployeesAmount[2] += 1;
+                }
+                else if ((customerSumma <= employeeSalary[2]) && (customerSumma >= employeeSalary[1]))
+                {
+                    customerSumma -= employeeSalary[1];
+                    EmployeesAmount[1] += 1;
+                }
+                else if ((customerSumma <= employeeSalary[1]) && (customerSumma >= employeeSalary[0]))
+                {
+                    customerSumma -= employeeSalary[0];
+                    EmployeesAmount[0] += 1;
+                }
+                if (customerSumma < employeeSalary[0])
+                {
+                    break;
+                }
+            }
+            if (new Manager().TotalEmployeesSalary(EmployeesAmount) == 0)
+            {
+                throw new CustomerMoneyException();
+            }
+            return EmployeesAmount;
+        }
+    }
+}
