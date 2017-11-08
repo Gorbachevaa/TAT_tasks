02b4a45f9e54ef7f7f97b8bd981ca2851c3802b8
Gorbachevaa <anya_gorbacheva96@mail.ru>
2017-11-08 17:14:50 +0300

    Add EntryPoint.

--- Dev-13/EntryPoint.cs
+++ Dev-13/EntryPoint.cs
@@ -2,27 +2,24 @@
 
 namespace Dev_13
 {
-    enum TypeOfEmployees
-    {
-        Junior,
-        Middle,
-        Senior,
-        Lead
-    }
     class EntryPoint
     {
         const string DATAPROMPT = "Input 1 = the amount of money, \n2 = the required productivity: ";
         const string INPUTERROR = "Bad value. Please, try to enter again.";
-        const int CUSTOMERSINFOSIZE = 2;
+        const string CRITERIONPROMPT = @"Input a selection criterion. 
+          1 - maximum productivity at the fixed price. 
+          2 - minimum summa at the fixed productivity. 
+          3 - minimum amount of high qualified employees. Productivity is fixed.";
+        const int CUSTOMERINFOSIZE = 2;
         static void Main(string[] args)
         {
             try
             {
                 /// Input info from customer.
                 Console.WriteLine(DATAPROMPT);
-                int[] customerInfo = new int[CUSTOMERSINFOSIZE];
+                int[] customerInfo = new int[CUSTOMERINFOSIZE];
                 int i = 0;
-                while (i < CUSTOMERSINFOSIZE)
+                while (i < CUSTOMERINFOSIZE)
                 {
                     Console.Write(i + 1 + " = ");
                     if (int.TryParse(Console.ReadLine(), out customerInfo[i]))
@@ -32,12 +29,23 @@ namespace Dev_13
                     }
                     Console.WriteLine(INPUTERROR);
                 }
-                EmployeeHandler handler = new JuniorHandler(
-                    new MiddleHandler(
-                    new SeniorHandler(
-                    new LeadHandler(null))));
-                Employee employee = handler.Handle(customerInfo);
-                Console.WriteLine(employee.GetEmployeeType());
+                Console.WriteLine(CRITERIONPROMPT);
+                i = 0;
+                /// Input the selection criterion.
+                while ( i < 1 )
+                {
+                    int criterionNumb;
+                    if (int.TryParse(Console.ReadLine(), out criterionNumb))
+                    {
+                        Customer customer = new Customer(customerInfo,
+                        new CriterionHandler(criterionNumb).PickTheCriterion());
+                        Manager manager = new Manager();
+                        manager.GetEmployees(customer.PickATeam(customerInfo));
+                        i++;
+                        continue;
+                    }
+                    throw new FormatException();
+                }
             }
             catch (Exception ex)
             {
